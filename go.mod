935810e785c97341b984ea284a66213f3d618244
Wenpiner <wenpiner@gmail.com>
2025-03-26 02:31:11 +0800

    refactor(deps): 👌 replace deprecated ioutil with os package
    - Replace ioutil.ReadFile with os.ReadFile
    - Remove deprecated ioutil import
    - Add os package import

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module WProxy
 go 1.20
 
 require golang.org/x/net v0.14.0
+
+require gopkg.in/yaml.v3 v3.0.1
--- proxy/proxy.go
+++ proxy/proxy.go
@@ -9,26 +9,84 @@ import (
 	"log"
 	"net"
 	"net/url"
+	"os"
+	"strconv"
+
+	"gopkg.in/yaml.v3"
 )
+
+type Config struct {
+	ListenAddr string `yaml:"listen_addr"`
+	Username   string `yaml:"username"`
+	Password   string `yaml:"password"`
+}
 
 func main() {
 	host := flag.String("host", "0.0.0.0", "host")
 	port := flag.Int("port", 1080, "port")
 	username := flag.String("username", "", "username")
 	password := flag.String("password", "", "password")
+	configFile := flag.String("c", "", "config file path")
 	flag.Parse()
+
+	var config Config
+	if *configFile != "" {
+		// Read config file
+		data, err := os.ReadFile(*configFile)
+		if err != nil {
+			log.Fatalln("Failed to read config file:", err)
+			return
+		}
+		err = yaml.Unmarshal(data, &config)
+		if err != nil {
+			log.Fatalln("Failed to parse config file:", err)
+			return
+		}
+		// Override config with command line arguments if provided
+		if *host != "0.0.0.0" {
+			config.ListenAddr = *host
+		}
+		if *port != 1080 {
+			config.ListenAddr = fmt.Sprintf("%s:%d", config.ListenAddr, *port)
+		}
+		if *username != "" {
+			config.Username = *username
+		}
+		if *password != "" {
+			config.Password = *password
+		}
+	} else {
+		// Use command line arguments
+		config.ListenAddr = fmt.Sprintf("%s:%d", *host, *port)
+		config.Username = *username
+		config.Password = *password
+	}
+
+	// Parse listen address
+	hostStr, portStr, err := net.SplitHostPort(config.ListenAddr)
+	if err != nil {
+		log.Fatalln("Invalid listen address:", err)
+		return
+	}
+	portNum, err := strconv.Atoi(portStr)
+	if err != nil {
+		log.Fatalln("Invalid port:", err)
+		return
+	}
+
 	listen, err := net.ListenTCP("tcp", &net.TCPAddr{
-		IP:   net.ParseIP(*host),
-		Port: *port,
+		IP:   net.ParseIP(hostStr),
+		Port: portNum,
 	})
 	if err != nil {
 		log.Fatalln("Failed to start TCP proxy:", err)
 		return
 	}
+
 	var urlinfo *url.Userinfo
-	if *username != "" && *password != "" {
-		fmt.Println("TCP proxy started successfully with credentials:", *username, *password)
-		urlinfo = url.UserPassword(*username, *password)
+	if config.Username != "" && config.Password != "" {
+		fmt.Println("TCP proxy started successfully with credentials:", config.Username, config.Password)
+		urlinfo = url.UserPassword(config.Username, config.Password)
 	} else {
 		fmt.Println("TCP proxy started successfully without credentials")
 	}
